444af9c90022db931d35c79616d2d19881da07fa
Bayu Utomo <by.ciikel@gmail.com>
2020-03-01 00:50:13 +0700

    style(chat): create socket to actioncable

--- src/Chat.vue
+++ src/Chat.vue
@@ -6,3 +6,31 @@
     <button>Send</button>
   </div>
 </template>
+
+<script>
+import Cable from 'actioncable'
+
+let cable = Cable.createConsumer('http://api-rails-chat.herokuapp.com/cable');
+let createSocket = cable.subscriptions.create({
+      channel: 'ChatChannel'
+    }, {
+      initialize: function() {},
+      connected: function() {},
+      received: (data) => {
+        console.log(data)
+      },
+      create: function(chatContent) {
+        this.perform('create', {
+          message: chatContent
+        });
+      },
+      history: function() {
+        return this.perform('history')
+      }
+    });
+  
+export default {
+  created() {
+  },
+}
+</script>
