6db61ef813078d426a86f85eddd6988da7ed1483
Bayu Utomo <by.ciikel@gmail.com>
2020-03-01 00:33:35 +0700

    feat: add Chat

---
+++ src/Chat.vue
@@ -0,0 +1,8 @@
+<template>
+  <div id="container">
+    <h1>Chat Room</h1>
+    <div id="chat-logs"></div>
+    <input type="text" placeholder="type something..." />
+    <button>Send</button>
+  </div>
+</template>
